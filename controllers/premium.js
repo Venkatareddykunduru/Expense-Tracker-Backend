f4bf89b184d9b40adc57a14df76f8bd55c10a5dd
Venkatareddykunduru <venkatareddykunduru123@gmail.com>
2024-07-19 20:00:04 +0530

    optimized the code for getting leaderboard details

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,27 +1,30 @@
-const User=require('../models/user');
-const Expense=require('../models/expense');
+const { Sequelize } = require('sequelize');
+const User = require('../models/user');
+const Expense = require('../models/expense');
 
-exports.showleaderboard=async (req, res) => {
+exports.showleaderboard = async (req, res) => {
     try {
         if (!req.user.ispremium) {
             return res.status(403).json({ message: 'Premium membership required' });
         }
 
-        const users = await User.findAll({
-            attributes: ['name', 'email'],
+        const leaderboard = await User.findAll({
+            attributes: [
+                'name', 
+                'email',
+                [Sequelize.fn('COALESCE', Sequelize.fn('SUM', Sequelize.col('expenses.amount')), 0), 'totalExpense']
+            ],
             include: [{
                 model: Expense,
-                attributes: ['amount']
-            }]
+                attributes: []
+            }],
+            group: ['User.id'],
+            order: [[Sequelize.literal('totalExpense'), 'DESC']]
         });
-
-        const leaderboard = users.map(user => {
-            const totalExpense = user.expenses.reduce((sum, expense) => sum + expense.amount, 0);
-            return { name: user.name, email: user.email, totalExpense };
-        }).sort((a, b) => b.totalExpense - a.totalExpense);
 
         res.json(leaderboard);
     } catch (error) {
+        console.error('Error fetching leaderboard:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
-}
+};
